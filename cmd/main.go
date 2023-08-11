1fcf04fe976217128dbf1f567901e8d7c230729d
thiago.silva <thiago.sdd@live.com>
2023-08-11 13:57:31 -0300

    feat: add logger package

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/thiago-s-silva/ms-go-ddd-rest-api/config"
+	"github.com/thiago-s-silva/ms-go-ddd-rest-api/pkg"
 )
 
 func main() {
+	// init Logger
+	logger := pkg.NewLogger("main.go")
+
 	// init Gin Gonic
 	server := gin.Default()
 
 	// init Config
 	cfg, err := config.Load()
 	if err != nil {
+		logger.Errorf("error when tried to load config package: %v", err)
 		panic(err)
 	}
 
